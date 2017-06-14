131117ae7869aafd43213be5c6b58c775dd798ff
Bui Quang Tuan <Bui Quang Tuan>
2017-06-14 23:21:58 +0700

    First commit

---
+++ src/ball.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View } from 'react-native';
+
+class Ball extends React.Component {
+    render() {
+        return (
+            <View style={styles.ball}/>
+        );
+    }
+}
+
+const styles = {
+    ball: {
+        height: 60,
+        width: 60,
+        borderRadius: 30,
+        borderWidth: 30,
+        borderColor: 'black'
+    }
+}
+
+export default Ball;
