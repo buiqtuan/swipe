6bee02c496e034031230876f8e2717ff66337fbb
Bui Quang Tuan <Bui Quang Tuan>
2017-06-15 00:35:05 +0700

    Adding animation

--- src/ball.js
+++ src/ball.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
 
 class Ball extends React.Component {
+    componentWillMount() {
+        this.position = new Animated.ValueXY(0,0);
+
+        Animated.spring(this.position, {
+            toValue: { x: 200, y: 500 }
+        }).start();
+    }
+
     render() {
         return (
-            <View style={styles.ball}/>
+            <Animated.View style={this.position.getLayout()}>
+                <View style={styles.ball}/>
+            </Animated.View>
         );
     }
 }
